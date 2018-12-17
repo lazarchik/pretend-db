bfbd195b7dbf9d994a008f4ce0469ebcafef694d
Eugene Lazarchik <eugenel@care2team.com>
2018-12-16 18:58:13 -0800

    Better exception output.
    Doctrine doesn't preserve "caused by" exceptions so we have to print them into a string before throwing.

--- src/PretendDb/Doctrine/Driver/MySQLServer.php
+++ src/PretendDb/Doctrine/Driver/MySQLServer.php
@@ -128,7 +128,12 @@ class MySQLServer
             }
             
         } catch (\Exception $e) {
-            throw new \RuntimeException("Can't execute query: ".$queryString."\n\nMessage:\n".$e->getMessage(), 0, $e);
+            throw new \RuntimeException(
+                "Can't execute query: ".$queryString
+                ."\n\nException at ".$e->getFile().":".$e->getLine().":\n"
+                .$e->getMessage()."\n"
+                .$this->exceptionTracePrettyPrint($e)
+            );
         }
         
         throw new \RuntimeException(
@@ -695,5 +700,62 @@ class MySQLServer
         }
         
         return $parsedStatement;
+    }
+    
+    /**
+     * Provide a Java style exception trace
+     * From https://secure.php.net/manual/en/exception.gettraceasstring.php
+     * @param \Exception $exception
+     * @param array|null $seen - array passed to recursive calls to accumulate trace lines already seen
+     *                     leave as NULL when calling this function
+     * @return string
+     */
+    protected function exceptionTracePrettyPrint(\Exception $e, ?array $seen = null): string
+    {
+        $starter = $seen ? 'Caused by: ' : '';
+        if (!$seen) {
+            $seen = [];
+        }
+        $trace = $e->getTrace();
+        $prev = $e->getPrevious();
+        $result = sprintf('%s%s: %s', $starter, get_class($e), $e->getMessage());
+        $file = $e->getFile();
+        $line = $e->getLine();
+        
+        while (true) {
+            $current = "$file:$line";
+            if (is_array($seen) && in_array($current, $seen)) {
+                $result .= sprintf(' ... %d more', count($trace) + 1);
+                break;
+            }
+            $result .= sprintf(
+                ' at %s%s%s(%s%s%s)',
+                count($trace) && array_key_exists('class', $trace[0])
+                    ? str_replace('\\', '.', $trace[0]['class']) : '',
+                count($trace) && array_key_exists('class', $trace[0]) && array_key_exists('function', $trace[0])
+                    ? '.' : '',
+                count($trace) && array_key_exists('function', $trace[0])
+                    ? str_replace('\\', '.', $trace[0]['function']) : '(main)',
+                $line === null ? $file : basename($file),
+                $line === null ? '' : ':',
+                $line === null ? '' : $line
+            );
+            if (is_array($seen)) {
+                $seen[] = "$file:$line";
+            }
+            if (!count($trace)) {
+                break;
+            }
+            $file = array_key_exists('file', $trace[0]) ? $trace[0]['file'] : 'Unknown Source';
+            $line = array_key_exists('file', $trace[0]) && array_key_exists('line', $trace[0]) && $trace[0]['line']
+                ? $trace[0]['line'] : null;
+            array_shift($trace);
+        }
+        
+        if ($prev) {
+            $result .= "\n".$this->exceptionTracePrettyPrint($prev, $seen);
+        }
+    
+        return $result;
     }
 }
