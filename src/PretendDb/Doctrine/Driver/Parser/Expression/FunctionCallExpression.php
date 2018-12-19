fc8b4651d0609a7d6dc13c1835fff9fb98bc4624
Eugene Lazarchik <eugenel@care2team.com>
2018-12-19 00:37:40 -0800

    Implemented IF() and UPPER().

--- src/PretendDb/Doctrine/Driver/Parser/Expression/FunctionCallExpression.php
+++ src/PretendDb/Doctrine/Driver/Parser/Expression/FunctionCallExpression.php
@@ -22,7 +22,29 @@ class FunctionCallExpression extends AbstractExpression
     
     public function evaluate(EvaluationContext $evaluationContext)
     {
-        throw new \RuntimeException("Method not implemented");
+        switch (strtoupper($this->functionName)) {
+            case "IF":
+                $this->assertArgumentCount(3);
+                return $this->arguments[0]->evaluate($evaluationContext)
+                    ? $this->arguments[1]->evaluate($evaluationContext)
+                    : $this->arguments[2]->evaluate($evaluationContext);
+            case "UPPER":
+                $this->assertArgumentCount(1);
+                return strtoupper($this->arguments[0]->evaluate($evaluationContext));
+            default:
+                throw new \RuntimeException("Function ".$this->functionName." is not implemented");
+        }
+    }
+    
+    protected function assertArgumentCount(int $expectedArgumentCount): void
+    {
+        $actualArgumentCount = count($this->arguments);
+        if ($expectedArgumentCount != $actualArgumentCount) {
+            throw new \RuntimeException(
+                "Expected ".$expectedArgumentCount." arguments for function ".$this->functionName
+                    .", got: ".$actualArgumentCount
+            );
+        }
     }
 
     public function dump(string $indentationString = ""): string
